97e0675ff5887a0f65f2f54038b5f91788177e2c
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-07 22:18:32 -0500

    creando la función para listar productos y consumiento el funcionamiento realizado en el archivo de listar.php

--- productos_php_api_fecth/assets/script_fecth.js
+++ productos_php_api_fecth/assets/script_fecth.js
@@ -4,23 +4,33 @@
    esta promesa them se conecta con el archivo de registrar.php 
    este "ok" hace referencia al echo definido al final del script del archivo registrar.php
 */
-try {
-    registrar.addEventListener("click", () => {
-        fetch("/productos_php_api_fecth/src/registrar.php", {
-            method: "POST",
-            body: new FormData(formulario)
-        }).then(response => response.text()).then(response => {   
-            if(response == "ok"){                               
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Producto Registrado Correctamente',
-                    showConfirmButton: false,
-                    timer: 1500
-                })
-                formulario.reset();
-            }
-        })
+ListarProductos();
+function ListarProductos(busqueda) {
+  fetch("listar.php", {
+    method: "POST",
+    body: busqueda,
+  })
+    .then((response) => response.text())
+    .then((response) => {
+      resultado.innerHTML = response;
     });
-} catch (error) {
-    console.log(error);
 }
+
+registrar.addEventListener("click", () => {
+  fetch("/productos_php_api_fecth/src/registrar.php", {
+    method: "POST",
+    body: new FormData(formulario),
+  })
+    .then((response) => response.text())
+    .then((response) => {
+      if (response == "ok") {
+        Swal.fire({
+          icon: "success",
+          title: "Producto Registrado Correctamente",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        formulario.reset();
+      }
+    });
+});
