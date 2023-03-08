b89ad7b04a48f56b3420fd7c607c28b965d7b195
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-07 22:12:32 -0500

    ingresando las rutas respectivas para hacer uso de la base de datos y redireccionando desde el script_fetch hacia el archivo registrar.php

--- productos_php_api_fecth/assets/script_fecth.js
+++ productos_php_api_fecth/assets/script_fecth.js
@@ -1,19 +1,26 @@
 /* capturando el id del boton del formulario
    formulario_productos -> hace referencia al id que se le da al formulario
+
+   esta promesa them se conecta con el archivo de registrar.php 
+   este "ok" hace referencia al echo definido al final del script del archivo registrar.php
 */
-registrar.addEventListener("click", () => {
-    fetch("../src/registrar.php", {
-        method: "POST",
-        body: new FormData(formulario)
-    }).then(response => response.text()).then(response => {   /* esta promesa se conecta con el archivo de registrar.php */
-        if(response == "ok"){                                /* este ok hace referencia al echo definido al final del script del archivo registrar.php*/
-            Swal.fire({
-                icon: 'success',
-                title: 'Producto Registrado Correctamente',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            formulario.reset();
-        }
-    })
-});
+try {
+    registrar.addEventListener("click", () => {
+        fetch("/productos_php_api_fecth/src/registrar.php", {
+            method: "POST",
+            body: new FormData(formulario)
+        }).then(response => response.text()).then(response => {   
+            if(response == "ok"){                               
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Producto Registrado Correctamente',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                formulario.reset();
+            }
+        })
+    });
+} catch (error) {
+    console.log(error);
+}
--- productos_php_api_fecth/src/registrar.php
+++ productos_php_api_fecth/src/registrar.php
@@ -2,22 +2,26 @@
     /* Hacemos una validación de si existe $_POST
         Optenemos los name de cada input del formulario
      */
-    if(isset($_POST)){
-        $codigo = $_POST['codigo'];
-        $producto = $_POST['producto'];
-        $precio = $_POST['precio'];
-        $cantidad = $_POST['cantidad'];
-        $valor_total = $precio * $cantidad;
-        require("../database/conexion.php");
-        $query = $pdo->prepare("INSERT INTO productos(codigo,producto,precio,cantidad,valor_total)VALUES(:codigo,:producto,:precio,:cantidad,:valor_total)");
-        $query->bindParam(":codigo", $codigo);
-        $query->bindParam(":producto", $producto);
-        $query->bindParam(":precio", $precio);
-        $query->bindParam(":cantidad", $cantidad);
-        $query->bindParam(":valor_total", $valor_total);
-        $query->execute();
-        $pdo = null;
-        echo "ok";
+    try {
+        if(isset($_POST)){
+            $codigo = $_POST['codigo'];
+            $producto = $_POST['producto'];
+            $precio = $_POST['precio'];
+            $cantidad = $_POST['cantidad'];
+            $valor_total = ($precio * $cantidad);
+            require("../database/conexion.php");
+            $query = $pdo->prepare("INSERT INTO productos(codigo,producto,precio,cantidad,valor_total)VALUES(:cod,:prod,:prec,:cant,:vtotal)");
+            $query->bindParam(":cod", $codigo);
+            $query->bindParam(":prod", $producto);
+            $query->bindParam(":prec", $precio);
+            $query->bindParam(":cant", $cantidad);
+            $query->bindParam(":vtotal", $valor_total);
+            $query->execute();
+            $pdo = null;
+            echo "ok";
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
 
 
