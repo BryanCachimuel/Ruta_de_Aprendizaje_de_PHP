9b66f640c2d364aced6c621bfa4dda7624f64bec
BryanCachimuel <bryanloyo56@gmail.com>
2023-03-08 20:18:27 -0500

    creando el script necesario en el archivo script_fecth.js para poder eliminar un registro seleccionado

--- productos_php_api_fecth/assets/script_fecth.js
+++ productos_php_api_fecth/assets/script_fecth.js
@@ -35,6 +35,37 @@ registrar.addEventListener("click", () => {
     });
 });
 
+function Eliminar(id) {
+  Swal.fire({
+      title: 'Esta seguro de eliminar?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si!',
+      cancelButtonText: 'NO'
+  }).then((result) => {
+      if (result.isConfirmed) {
+          fetch("eliminar.php", {
+              method: "POST",
+              body: id
+          }).then(response => response.text()).then(response => {
+              if (response == "ok") {
+                 ListarProductos();
+                 Swal.fire({
+                     icon: 'success',
+                     title: 'Eliminado',
+                     showConfirmButton: false,
+                     timer: 1500
+                 })
+              }
+              
+          })
+          
+      }
+  })
+}
+
 function Editar(id) {
     fetch("editar.php", {
         method: "POST",
