35d334fbc0e321dfba023454fc8d353ba44124ec
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-27 22:20:09 -0500

    creando un método con el nombre guardar el cual se encargará de mandar los datos desde el controlador y almacenarlos en el método establecido en UsuariosModel para guardar un nuevo registro

--- crud_codeignater/app/Controllers/Usuarios/Add.php
+++ crud_codeignater/app/Controllers/Usuarios/Add.php
@@ -2,10 +2,23 @@
 
 namespace App\Controllers\Usuarios;
 use CodeIgniter\Controller;
+use App\Models\UsuarioModel;
 
 class Add extends Controller {
 
     public function index() {
         print_r("Hola");
+    }
+
+    public function guardar() {
+        $usuarioModel = new UsuarioModel();
+        
+        $data = [
+            "nombre" => "Bryan Cachimuel",
+            "emil" => "bryancachimuel@gmail.com",
+            "bibliografica" => "Soy un Ingeniero en Sistemas Computacionales"
+        ];
+
+        $usuarioModel->crearUsuario($data);
     }
 }
