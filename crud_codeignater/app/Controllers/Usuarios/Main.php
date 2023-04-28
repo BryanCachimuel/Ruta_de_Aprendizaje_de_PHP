70fc39762bef8d3ee6fa1a35113ce4f704798241
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-27 22:25:10 -0500

    desarrollando el controlador para poder listar todos los registros realizados

---
+++ crud_codeignater/app/Controllers/Usuarios/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers\Usuarios;
+use CodeIgniter\Controller;
+use App\Models\UsuarioModel;
+
+class Main extends Controller {
+
+    public function index() {
+        $usuarioModel = new UsuarioModel();
+        print_r($usuarioModel->listarRegistros());
+    }
+
+
+}
