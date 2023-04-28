744a26e823f27d7736d1fdad942f102539a31d12
BryanCachimuel <bryanloyo56@gmail.com>
2023-04-27 22:37:07 -0500

    creando el método para eliminar un registro mediante su id

--- crud_codeignater/app/Models/UsuarioModel.php
+++ crud_codeignater/app/Models/UsuarioModel.php
@@ -7,9 +7,25 @@ class UsuarioModel extends Model {
     protected $primaryKey = 'id';
     protected $allowedFields = ['nombre','email','bibliografia'];
 
+
+    /* Listar los Usuarios Registrados */
+    public function listarRegistros(){
+        return $this->findAll();
+    }
+
     /* insertar los datos enviados desde el controller a la tabla */
     public function crearUsuario($data){
         return $this->insert($data);
+    }
+
+    /* actualizar un registro */
+    public function actualizarUsuario($id, $data){
+        return $this->update($id, $data);
+    }
+
+    /* eliminar un usuario */
+    public function eliminarUsuario($id){
+        return $this->delete($id);
     }
 
 }
