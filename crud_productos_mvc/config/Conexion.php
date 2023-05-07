f80deeff86740774ad2b13a2208f9339a9b6fa47
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-06 20:02:14 -0500

    configurando la conexión hacia la base de datos mysql

---
+++ crud_productos_mvc/config/Conexion.php
@@ -0,0 +1,20 @@
+<?php
+    class Conectar{
+        protected $base_datos;
+
+        protected function Conexion(){
+            try {
+                $conectar = $this->base_datos = new PDO("mysql:local=localhost;dbname=crud_productos_mvc","root","");
+                return $conectar; 
+            } catch (Exception $e) {
+                print "¡Error en la Conexión hacia la base de datos: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+
+        /* función para reconocer las ñ y las tildes */
+        public function set_names(){
+            return $this->base_datos->query("SET NAMES 'utf8'");
+        }
+    }
+?>
