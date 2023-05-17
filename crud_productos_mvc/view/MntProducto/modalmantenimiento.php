45b15cb19f647789fb1274e3e88d3b8372fec667
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-16 19:32:19 -0500

    mejorando el modal para el registro de productos

--- crud_productos_mvc/view/MntProducto/modalmantenimiento.php
+++ crud_productos_mvc/view/MntProducto/modalmantenimiento.php
@@ -1,10 +1,7 @@
 <div id="modalmantenimiento" class="modal fade bd-example-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <!--<button type="button" class="modal-close" data-dismiss="modal" aria-lebel="Close">
-                    <i class="font-icon-close-2"></i>
-                </button>-->    
+            <div class="modal-header">   
                 <h4 class="modal-title" id="mdltitulo"></h4>
             </div>
             <form method="post" id="producto-form">
