ad02d33bbdaa6383f1d4f1de30f0df8f53684277
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-17 19:56:55 -0500

    realizando la reconección hacia la base de datos

--- crud_productos_mvc/config/Conexion.php
+++ crud_productos_mvc/config/Conexion.php
@@ -1,20 +1,20 @@
 <?php
     class Conectar{
-        protected $base_datos;
+        protected $dbh;
 
-        protected function conexion(){
-            try {
-                $conectar = $this->base_datos = new PDO("mysql:local=localhost;dbname=crud_productos_mvc","root","");
-                return $conectar; 
-            } catch (Exception $e) {
-                print "¡Error en la Conexión hacia la base de datos: " . $e->getMessage() . "<br/>";
+        protected function Conexion(){
+            try{
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=crud_productos_mvc","root","");
+                return $conectar;
+            }catch(Exception $e){
+                print "¡Error BD!: " . $e->getMessage() . "<br/>";
                 die();
             }
         }
 
-        /* función para reconocer las ñ y las tildes */
         public function set_names(){
-            return $this->base_datos->query("SET NAMES 'utf8'");
+			return $this->dbh->query("SET NAMES 'utf8'");
         }
+
     }
 ?>
