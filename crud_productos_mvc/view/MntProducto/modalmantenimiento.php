977148eb4d84e8737bb091ddde799dbded24d50d
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-19 20:44:13 -0500

    diseñando el modal para el ingreso y modificación de los registros de la base de datos3

--- crud_productos_mvc/view/MntProducto/modalmantenimiento.php
+++ crud_productos_mvc/view/MntProducto/modalmantenimiento.php
@@ -1,28 +1,17 @@
 <div id="modalmantenimiento" class="modal fade bd-example-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">   
-                <h4 class="modal-title" id="mdltitulo"></h4>
-            </div>
-            <form method="post" id="producto-form">
+            <form method="post" id="producto_form">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="mdltitulo"></h4>
+                </div>
                 <div class="modal-body">
-                    <input type="hidden" name="id_producto" id="id_producto">
+                    <input type="hidden" id="prod_id" name="prod_id">
 
                     <div class="form-group">
-                        <label class="form-label" for="nombre">Nombre: </label>
-                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingrese Nombre" required>
+                        <label class="form-label" for="prod_nom">Nombre</label>
+                        <input type="text" class="form-control" id="prod_nom" name="prod_nom" placeholder="Ingrese Nombre" required>
                     </div>
-
-                    <div class="form-group">
-                        <label class="form-label" for="precio">Precio: </label>
-                        <input type="text" class="form-control" id="precio" name="precio" placeholder="Ingrese Precio" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="form-label" for="cantidad">Cantidad: </label>
-                        <input type="text" class="form-control" id="cantidad" name="cantidad" placeholder="Ingrese Cantidad" required>
-                    </div>
-
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-rounded btn-default" data-dismiss="modal">Cerrar</button>
