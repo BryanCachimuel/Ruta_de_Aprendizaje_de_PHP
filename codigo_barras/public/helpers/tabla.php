82f2f1dffe0166782a7c51c5732bfda1f0551239
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-07 20:54:36 -0500

    creando la tabla donde se podrá ver las caracteristicas de los farmaceuticos registrados

---
+++ codigo_barras/public/helpers/tabla.php
@@ -0,0 +1,10 @@
+<table class="table">
+    <tr>
+        <td>Nombre del Farmaco</td>
+        <td>Código de Barras</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
