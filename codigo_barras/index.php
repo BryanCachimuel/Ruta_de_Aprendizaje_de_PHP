0c368403d27ed7e3c04e6d59ae3002894c971150
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-07 20:36:18 -0500

    creando la maquetación principal del generador de código de barras

--- codigo_barras/index.php
+++ codigo_barras/index.php
@@ -7,8 +7,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="public/lib/JsBarcode.all.min.js"></script>
   </head>
-  <body>
-    <h1>Código de Barras</h1>
+  <body> 
+    <div class="container">
+    <h1 class="mt-4">Código de Barras</h1>
+        <div class="row">
+            <div class="col-md-4">
+                <form action="" method="">
+                    <label for="">Nombre Farmaco:</label>
+                    <input type="text" name="nombre_farmaco" class="form-control">
+                    <br>
+                    <button class="btn btn-primary" type="submit">Generar Código de Barras</button>
+                </form>
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
