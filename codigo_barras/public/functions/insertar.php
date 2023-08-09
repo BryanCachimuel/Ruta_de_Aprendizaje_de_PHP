05372bad63f90b8dc0b6b8604747fdbbbb06d86c
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-08 21:02:14 -0500

    insertando los datos mediante consulta sql y realizando el script de registro hacia la base de datos

--- codigo_barras/public/functions/insertar.php
+++ codigo_barras/public/functions/insertar.php
@@ -1,13 +1,20 @@
 <?php
-    require_once "C:/xampp/htdocs/Ejercicios_PHP/codigo_barras/database/conexion.php";
+    require_once("C://xampp/htdocs/Ejercicios_PHP/codigo_barras/database/conexion.php");
     $conectar = conexion();
 
     $nombre = $_POST['nombre_farmaceutico'];
-    $sql = "INSERT INTO farmaceuticos(nombre_farmaceutico) VALUES($nombre)";
+    $sql = "INSERT INTO medicamentos(nombre_farmaceutico) VALUES('$nombre')";
 
     $resultado = mysqli_query($conectar, $sql);
 
     $id = mysqli_insert_id($conectar); // se obtiene el último id insertado
+    $codigo = $id.date('is');
 
-    header("Location:C:/xampp/htdocs/Ejercicios_PHP/codigo_barras/index.php");
+    $consulta = "UPDATE medicamentos SET codigo_farmaceutico='$codigo' WHERE id_farmaceutico='$id'";
+    mysqli_query($conectar,$consulta);
+
+    $url = "http://localhost/Ejercicios_PHP/codigo_barras/index.php";
+
+    header("Location: $url");
+    // C://xampp/htdocs/Ejercicios_PHP/codigo_barras/index.php
 ?>
