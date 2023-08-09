a320de1ab3a48a8d9bf2834acf3880ecc08193a2
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-08 21:03:14 -0500

    agregando el script para poder listar en la tabla los atributos correspondientes a los datos registrados de los medicamentos

--- codigo_barras/public/helpers/tabla.php
+++ codigo_barras/public/helpers/tabla.php
@@ -1,10 +1,24 @@
+<?php
+    require_once("C://xampp/htdocs/Ejercicios_PHP/codigo_barras/database/conexion.php");
+    $conectar = conexion();
+    $sql = "SELECT * FROM medicamentos";
+    $resultado = mysqli_query($conectar,$sql);
+    $arraycodigos = array();
+?>
+
 <table class="table">
     <tr>
         <td>Nombre del Farmaco</td>
         <td>Código de Barras</td>
     </tr>
-    <tr>
-        <td></td>
-        <td></td>
-    </tr>
+    <?php while($datos=mysqli_fetch_row($resultado)) : 
+          $arraycodigos[] = (string)$datos[2];    
+    ?>
+        <tr>
+            <td><?php echo $datos[1]; ?></td>
+            <td>
+                <svg id="<?php echo "barcode".$datos[2] ?>">
+            </td>
+        </tr>
+    <?php endwhile;  ?>
 </table>
