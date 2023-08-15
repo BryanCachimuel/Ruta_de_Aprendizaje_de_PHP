2780c86983225e3fcb06f4aadfc22136eb28be8f
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-14 22:03:42 -0500

    creando la vista para eliminar un registro y realizando la maquetación y diseño respectivo

---
+++ crud_php_mongodb/views/eliminar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eliminar</title>
+    <link rel="stylesheet" href="../public/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card mt-3 fondoEliminacion" style="background-color: #fa795f;">
+                    <div class="card-header">
+                        <h2>Eliminar Registro</h2>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <th>Apellido Paterno</th>
+                                <th>Apellido Materno</th>
+                                <th>Nombres</th>
+                                <th>Fecha de Nacimiento</th>
+                                <th>Edad</th>
+                                <th>Ocupación</th>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <hr>
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <p>¿Estás Seguro de Eliminar este Registro?</p>
+                            <p>Una vez Eliminado no podrá ser recuperado</p>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        <form action="" method="POST">
+                            <button class="btn btn-danger"><i class="fa-solid fa-trash"></i> Eliminar</button>
+                            <a href="../index.php" class="btn btn-warning"><i class="fa-solid fa-xmark"></i> Cancelar</a>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="../public/bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
