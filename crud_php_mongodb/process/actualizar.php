ee90e234b41b056b3df0f61ccef2ffc63204cd9f
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-17 19:57:17 -0500

    obteniendo los datos que se van a actualizar del formulario de actualización y con esos datos actualizar el registro seleccionado mediante la función actualizar determinada en el archivo Crud.php

---
+++ crud_php_mongodb/process/actualizar.php
@@ -0,0 +1,25 @@
+<?php
+    include("../class/Conexion.php");
+    include("../class/Crud.php");
+
+    $crud = new Crud();
+
+    $id = $_POST['id'];
+
+    $datos = array(
+        "apellido_paterno" => $_POST['apellido_paterno'],
+        "apellido_materno" => $_POST['apellido_materno'],
+        "nombres" => $_POST['nombres'],
+        "fecha_nacimiento" => $_POST['fecha_nacimiento'],
+        "edad" => $_POST['edad'],
+        "ocupacion" => $_POST['ocupacion']
+    );
+
+    $respuesta = $crud->actualizar($id, $datos);
+
+    if($respuesta->getModifiedCount() > 0 || $respuesta->getMatchedCount()){
+        header("location:../index.php");
+    }else{
+        print_r($respuesta);
+    }
+?>
