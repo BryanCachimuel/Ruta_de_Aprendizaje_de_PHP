d1ad70cc1599f8ff57e625f8a55b84c24f1e71e5
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-29 22:21:34 -0500

    creando una nueva ruta la cual manda a llamar al controlador VacacionesController.php

--- cursos_vacacionales/routes/web.php
+++ cursos_vacacionales/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VacacionalesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| "index"-> nombre del método en el archivo VacacionalController
+| name("crud.index") -> nombre que se le da a la ruta
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/", [VacacionalesController::class, "index"])->name("crud.index");
