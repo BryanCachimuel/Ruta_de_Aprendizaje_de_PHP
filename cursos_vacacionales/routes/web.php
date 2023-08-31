9655f0e14f35429f9025215546c8c9364ccdc8f6
BryanCachimuel <bryanloyo56@gmail.com>
2023-08-30 21:54:21 -0500

    creando la ruta post para la función de create

--- cursos_vacacionales/routes/web.php
+++ cursos_vacacionales/routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", [VacacionalesController::class, "index"])->name("crud.index");
+
+// ruta para registra un nuevo estudiante
+Route::post("/registrar-estudiante", [VacacionalesController::class, "create"])->name("crud.create");
