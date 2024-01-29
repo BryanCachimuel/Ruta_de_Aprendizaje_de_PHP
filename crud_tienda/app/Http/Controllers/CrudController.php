aec7185fd0138d53e1dbb3b60187ff60fa90c12d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-01-28 21:32:01 -0500

    creando las funciones para cumplir con el crud en este caso se realizandos las funciones para crear, modificar y eliminar

--- crud_tienda/app/Http/Controllers/CrudController.php
+++ crud_tienda/app/Http/Controllers/CrudController.php
@@ -16,4 +16,58 @@ class CrudController extends Controller
         $datos = DB::select("SELECT * FROM producto");
         return view("welcome")->with("datos",$datos);
     }
+
+    /*TODO: para obtener todos los datos que se ingresen por el formulario se debe poner como parámetro de la función Request request */
+    public function create(Request $request){
+        try {
+            $sql = DB::insert("INSERT INTO producto(nombre,precio,cantidad) VALUES(?,?,?)",[
+                $request->txtNombre,
+                $request->txtPrecio,
+                $request->txtCantidad
+             ]);
+        } catch (\Throwable $th) {
+            $sql = 0;
+        }
+        if($sql == true){
+            /* si se registro correctamente se redirecciona con un mensaje que el producto se a registrado */
+            return back()->with("correcto","Producto registrado correctamente");
+        }else{
+            return back()->with("incorrecto","Error la registrar el Producto");
+        }
+    }
+
+    /*TODO: función para modificar los atributos de un registro realizado */
+    public function update(Request $request){
+        try {
+            $sql = DB::update("UPDATE producto SET nombre=?,precio=?,cantidad=? WHERE id_producto=?",[
+                $request->txtNombre,
+                $request->txtPrecio,
+                $request->txtCantidad,
+                $request->txtCodigo
+            ]);
+            if($sql == 0){
+                $sql = 1;
+            }
+        } catch (\Throwable $th) {
+            $sql = 0;
+        }
+        if($sql == true){
+            return back()->with("correcto","Producto Modificado correctamente");
+        }else{
+            return back()->with("incorrecto","Error al modificar el Producto");
+        }
+    }
+
+    public function delete($id){
+        try {
+            $sql = DB::delete("DELETE FROM producto WHERE id_producto=$id");
+        } catch (\Throwable $th) {
+            $sql = 0;
+        }
+        if($sql == true){
+            return back()->with("correcto","Producto Eliminado correctamente");
+        }else{
+            return back()->with("incorrecto","Error al eliminar el Producto");
+        }
+    }
 }
