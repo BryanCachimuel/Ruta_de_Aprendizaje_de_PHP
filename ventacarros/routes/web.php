535c3f49b32833abda177ec392606104480a3842
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-02-02 22:10:30 -0500

    agregando las rutas para marcas y carros

--- ventacarros/routes/web.php
+++ ventacarros/routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\MarcaController;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+    Web Routes
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('marcas', MarcaController::class);
+Route::resource('carros', CarroController::class);
