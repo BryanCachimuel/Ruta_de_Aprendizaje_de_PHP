bfbe91024ce98bf1bebc49468069185a42aec933
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-02-03 21:49:42 -0500

    realizando el modal de eliminación y adecuando las caracteristicas respectivas para eliminar un atributo seleccionado

--- ventacarros/resources/views/marca/info.blade.php
+++ ventacarros/resources/views/marca/info.blade.php
@@ -33,3 +33,34 @@
         </div>
     </div>
 </div>
+
+ <!-- Modal para eliminar -->
+ <div class="modal fade" id="eliminarmarca{{$marca->id_marca}}" tabindex="-1" aria-labelledby="eliminarmarcaLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminar Datos de Marca</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+               <form action="{{route('marcas.destroy',$marca->id_marca)}}" method="post" autocomplete="off">
+                   @csrf
+                   @method('DELETE')
+                   <div class="modal-body">
+
+                       <div class="mb-3">
+                          <p>
+                            ¿Está usted seguro de eliminar a la marca <strong>{{$marca->nombre_marca}}</strong>?
+                          </p>
+                       </div>
+                   </div>
+
+                   <div class="modal-footer">
+                       <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                       <button type="submit" class="btn btn-primary">Confirmar</button>
+                   </div>
+
+               </form>
+        </div>
+    </div>
+</div>
