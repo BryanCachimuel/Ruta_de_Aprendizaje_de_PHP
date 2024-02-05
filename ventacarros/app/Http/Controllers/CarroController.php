195f6ae52332701203bcf8dfdef11bb10ebe2e94
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-02-04 21:17:55 -0500

    dando funcionalidad a los métodos para realizar el crud para entidad Carro

--- ventacarros/app/Http/Controllers/CarroController.php
+++ ventacarros/app/Http/Controllers/CarroController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carro;
+use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class CarroController extends Controller
@@ -13,7 +14,9 @@ class CarroController extends Controller
     public function index()
     {
         $carros = Carro::all();
-        return view('carro.index', compact('carros'));
+        /* para obtener todas las marcas y poder listarlas en el select de creación de carros */
+        $marcas = Marca::all();
+        return view('carro.index', compact('carros','marcas'));
     }
 
     /**
@@ -29,7 +32,15 @@ class CarroController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $carros = new Carro;
+        $carros->id_marca = $request->input('id_marca');
+        $carros->nombre_propietario = $request->input('nombre_propietario');
+        $carros->descripcion_carro = $request->input('descripcion_carro');
+        $carros->precio_carro = $request->input('precio_carro');
+        $carros->cantidad_carro = $request->input('cantidad_carro');
+        $carros->valor_total_carro = $carros->precio_carro * $carros->cantidad_carro;
+        $carros->save();
+        return redirect()->back();
     }
 
     /**
@@ -51,16 +62,26 @@ class CarroController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Carro $carro)
+    public function update(Request $request, $id_carro)
     {
-        //
+        $carros = Carro::find($id_carro);
+        $carros->id_marca = $request->input('id_marca');
+        $carros->nombre_propietario = $request->input('nombre_propietario');
+        $carros->descripcion_carro = $request->input('descripcion_carro');
+        $carros->precio_carro = $request->input('precio_carro');
+        $carros->cantidad_carro = $request->input('cantidad_carro');
+        $carros->valor_total_carro = $carros->precio_carro * $carros->cantidad_carro;
+        $carros->update();
+        return redirect()->back();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Carro $carro)
+    public function destroy($id_carro)
     {
-        //
+        $carros = Carro::find($id_carro);
+        $carros->delete();
+        return redirect()->back();
     }
 }
