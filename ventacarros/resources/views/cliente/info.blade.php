367e301e60b2b917458697126489e69247da13b5
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-02-07 20:35:09 -0500

    añadiendo el modal para eliminar un registro seleccionado

--- ventacarros/resources/views/cliente/info.blade.php
+++ ventacarros/resources/views/cliente/info.blade.php
@@ -54,3 +54,30 @@
         </div>
     </div>
 </div>
+
+<!-- Modal para eliminar -->
+<div class="modal fade" id="eliminarcliente{{$cliente->id_cliente}}" tabindex="-1" aria-labelledby="eliminarclienteteLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminar Datos del Cliente</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <form action="{{ route('clientes.destroy',$cliente->id_cliente)}}" method="post" autocomplete="off">
+                @csrf
+                @method('DELETE')
+
+                <div class="modal-body">
+                    <p>¿Está seguro de querer eliminar al cliente: <strong>{{$cliente->nombre_cliente}}</strong>?</p>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary">Confirmar</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
