70a72c57ca2d719fca2d1d7bd1e0149c09fd2f7f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-19 21:22:31 -0500

    eliminando archivos innecesarios del repositorio local

--- codigo_barras/database/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function conexion(){
-        $conexion = mysqli_connect('localhost',
-                                   'root',
-                                   '',
-                                   'crud_codigo_barras');
-        return $conexion;
-    }
-?>
