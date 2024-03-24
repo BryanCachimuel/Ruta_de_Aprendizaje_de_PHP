1a3e39e91890d3e5bedfe0169a3ff8192b5dd746
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-23 22:16:08 -0500

    dentro del formulario integrando el monto total y el número de ticket como también agregando tres botones para procesar, limpiar y nueva consulta

--- regalos_clientes/index.php
+++ regalos_clientes/index.php
@@ -4,20 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Regalo Clientes</title>
-    <link rel="stylesheet" href="/css/estilos.css">
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
     <header>
         <h3 class="titulo">Regalo Clientes</h3>
     </header>
 
+    <?php 
+
+    error_reporting(0);
+
+    ?>
+
     <section>
         <form action="index.php" method="post">
-            <table>
+            <table class="tabla">
                 <tr>
                     <td>Nombre del Cliente: </td>
                     <td>
-                        <input class="input_datos" type="text" name="txtnombre" placeholder="Ingrese el nombre del cliente" value="<?php echo $_POST['txtNombre'] ?>">
+                        <input class="input_nombre" type="text" name="txtNombre" placeholder="Ingrese el nombre del cliente" value="<?php echo $_POST['txtNombre'] ?>">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Monto Total: </td>
+                    <td>
+                        <input class="input_monto" type="text" name="txtMonto" placeholder="Ingrese monto a pagar" value="<?php echo $_POST['txtMonto'] ?>">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Número de Ticket: </td>
+                    <td>
+                        <input class="input_ticket" type="text" name="txtTicket" placeholder="Ingrese número de tickey" value="<?php echo $_POST['txtTicket'] ?>">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td>
+                        <button class="btn_procesar" type="submit">Procesar</button>
+                        <button class="btn_limpiar" type="reset">Limpiar</button>
+                        <button class="btn_consulta" type="submit">Nueva Consulta</button>
                     </td>
                 </tr>
             </table>
