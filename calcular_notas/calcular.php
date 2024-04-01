77cf94889d2d5cf3dda3d2289dfc44988ea0069e
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-31 21:25:08 -0500

    procedimiento para el calculo de los valores que se ingresen denro de los inputs y realizando la sumatoria de los valores y obteniendo el promedio respectivo

--- calcular_notas/calcular.php
+++ calcular_notas/calcular.php
@@ -1,1 +1,23 @@
 <!-- captura de datos y calculo del promedio -->
+<?php
+
+if($_POST){
+    $cantidad = count($_POST);  /* se obtiene la cantidad de cajas de texto que se an introducido para calcular las notas */
+    $j = 1;                     /* para controlar el ciclo */ 
+    $sumar = 0;                 /* para sumar las notas */ 
+
+    while($j <= $cantidad){
+        $sumar += $_POST['txtNota'.$j];
+        $j++;
+    }
+
+    $promedio = $sumar/$cantidad;
+    $transformar_promedio = number_format($promedio,2,'.',',');
+    print "El promedio es: " . $transformar_promedio;
+}else{
+print "No permitido";
+}
+
+?>
+<br>
+<a href="index.php">Regresar</a>
