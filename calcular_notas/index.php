643d4475136e74cc8fa8e3cc6951ef7d48bd30cd
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-31 21:52:52 -0500

    integrando la ruta donde se encuentran los archivos de estilos y agregando clases a las etiquetas del formulario de ingreso de cantidad de notas

--- calcular_notas/index.php
+++ calcular_notas/index.php
@@ -5,42 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calcular Promedio</title>
+    <link rel="stylesheet" href="publico/estilos.css">
 </head>
 
 <body>
-    <form action="index.php" method="post" autocomplete="off">
-        <p>
-            Cantidad de Notas:
-            <input type="text" name="txtCantidad" id="txtCantidad">
-        </p>
-        <p>
-            <button type="submit">Ingresar Notas</button>
-        </p>
-    </form>
-
-    <?php
-    if ($_POST) {
-        $cantidad = $_POST['txtCantidad'];
-        $i = 1;                             /* contar el número de cajas de texto  */
-    ?>
-
-        <form action="calcular.php" method="post" autocomplete="off">
-            <?php
-
-            while ($i <= $cantidad) {             /* itera la cantidad de veces que se introduzca en la cantidad de cajas de texto que ingrese el usuario */
-            ?>
-                <p>
-                    Nota <?php print $i . ":" ?> <input type="text" name="txtNota<?php print $i ?>">
-                </p>
-            <?php
-                $i++;
-            }
-            ?>
-            <button type="submit">Calcular</button>
+    <div class="contenedor">
+        <h1 class="titulo">Calculador de Notas</h1>
+        <form class="formulario_cantidad" action="index.php" method="post" autocomplete="off">
+            <p>
+                Cantidad de Notas:
+                <input type="text" name="txtCantidad" id="txtCantidad">
+            </p>
+            <p>
+                <button class="boton_ingresar" type="submit">Ingresar Notas</button>
+            </p>
         </form>
-    <?php
-    }
-    ?>
+
+        <?php
+        if ($_POST) {
+            $cantidad = $_POST['txtCantidad'];
+            $i = 1;                             /* contar el número de cajas de texto  */
+        ?>
+
+            <form action="calcular.php" method="post" autocomplete="off">
+                <?php
+
+                while ($i <= $cantidad) {             /* itera la cantidad de veces que se introduzca en la cantidad de cajas de texto que ingrese el usuario */
+                ?>
+                    <p>
+                        Nota <?php print $i . ":" ?> <input type="text" name="txtNota<?php print $i ?>">
+                    </p>
+                <?php
+                    $i++;
+                }
+                ?>
+                <button type="submit">Calcular</button>
+            </form>
+        <?php
+        }
+        ?>
+    </div>
 </body>
 
 </html>
