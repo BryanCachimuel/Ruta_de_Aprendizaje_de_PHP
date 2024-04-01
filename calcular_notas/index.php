e4682f58d66b58baf5260ad5c4ebf49c0baace6f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-31 21:05:50 -0500

    formulario para el ingreso de la cantidad de notas que se requiera calcular

---
+++ calcular_notas/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcular Promedio</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <p>
+            Cantidad de Notas:
+            <input type="text" name="txtCantidad" id="txtCantidad">
+        </p>
+        <p>
+            <button type="submit">Ingresar Notas</button>
+        </p>
+    </form>
+
+    
+</body>
+
+</html>
