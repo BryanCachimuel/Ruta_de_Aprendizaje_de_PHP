d074891305687926bfdff9f1348911eb77520268
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-03-31 21:23:41 -0500

    formulario en donde se visualiza los inputs determinados por el usuario y también se manda a relizar los calculos de las notas que se ingresen dentro de los mismos

--- calcular_notas/index.php
+++ calcular_notas/index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
+    <form action="index.php" method="post" autocomplete="off">
         <p>
             Cantidad de Notas:
             <input type="text" name="txtCantidad" id="txtCantidad">
@@ -18,7 +18,29 @@
         </p>
     </form>
 
-    
+    <?php
+    if ($_POST) {
+        $cantidad = $_POST['txtCantidad'];
+        $i = 1;                             /* contar el número de cajas de texto  */
+    ?>
+
+        <form action="calcular.php" method="post" autocomplete="off">
+            <?php
+
+            while ($i <= $cantidad) {             /* itera la cantidad de veces que se introduzca en la cantidad de cajas de texto que ingrese el usuario */
+            ?>
+                <p>
+                    Nota <?php print $i . ":" ?> <input type="text" name="txtNota<?php print $i ?>">
+                </p>
+            <?php
+                $i++;
+            }
+            ?>
+            <button type="submit">Calcular</button>
+        </form>
+    <?php
+    }
+    ?>
 </body>
 
 </html>
