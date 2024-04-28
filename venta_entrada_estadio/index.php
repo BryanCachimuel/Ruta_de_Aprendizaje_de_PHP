8358b45d097d83efd86c79cf1448e625f5acda43
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-04-27 21:56:44 -0500

    realizando la validación del ingreso de información dentro de los inputs del formulario de registro de venta de entradas

--- venta_entrada_estadio/index.php
+++ venta_entrada_estadio/index.php
@@ -8,6 +8,35 @@
 </head>
 <body>
     <section>
+
+        <?php 
+
+            error_reporting();
+            $nombre_comprador = $_POST['txtComprador'];
+            $fecha_actual = $_POST['txtFecha'];
+            $cantidad_adultos = $_POST['txtEntradasAdultos'];
+            $cantidad_ninios = $_POST['txtEntradasNinios'];
+
+            $mComprador = '';
+            $mAdultos = '';
+            $mNinios = '';
+
+            if(isset($_POST['btnComprar'])){
+                if(empty($nombre_comprador)) $mComprador = 'Debe Ingresar el nombre del comprador';
+                elseif(is_numeric($mComprador)) $mComprador = 'Solo se permite letras en este campo';
+                else $mComprador;
+
+                if(empty($cantidad_adultos)) $mAdultos = 'Debe ingresar la cantidad de adultos';
+                elseif(!is_numeric($cantidad_adultos)) $mAdultos = 'Solo se permite números';
+                else $cantidad_adultos;
+
+                if(empty($cantidad_ninios)) $cantidad_ninios = 'Debe ingresar la cantidad de ninios';
+                elseif(!is_numeric($cantidad_ninios)) $cantidad_ninios = 'Solo se permite números';
+                else $cantidad_ninios;
+            }
+
+        ?>
+
         <form action="index.php" method="POST" autocomplete="off">
             <table>
                 <h4>Formulario de Compra de Entradas</h4>
@@ -17,7 +46,7 @@
                     <td>
                         <input type="text" name="txtComprador" class="comprador">
                     </td>
-                    <td width="200" id="error"></td>
+                    <td width="200" id="error"><?php echo $mComprador; ?></td>
                 </tr>
 
                 <tr>
@@ -30,23 +59,23 @@
                 <tr>
                     <td>Nro de Entradas para Adultos</td>
                     <td>
-                        <input type="text" name="txtNroEntradasAdultos" class="adultos">
+                        <input type="text" name="txtEntradasAdultos" class="adultos">
                     </td>
-                    <td width="200" id="error"></td>
+                    <td width="200" id="error"><?php echo $mAdultos; ?></td>
                 </tr>
 
                 <tr>
                     <td>Nro de Entradas para Niños</td>
                     <td>
-                        <input type="text" name="txtNroEntradasNinios" class="ninios">
+                        <input type="text" name="txtEntradasNinios" class="ninios">
                     </td>
-                    <td width="200" id="error"></td>
+                    <td width="200" id="error"><?php echo $mAdultos; ?></td>
                 </tr>
                 
                 <tr>
                     <td></td>
                     <td>
-                        <button class="boton" type="submit" name="btnAdquirir">Comprar Entradas</button>
+                        <button class="boton" type="submit" name="btnComprar">Comprar Entradas</button>
                         <button class="limpiar" type="reset">Limpiar</button>
                         <a href="index.php" class="nuevaCompra">Nueva Compra</a>
                     </td>
