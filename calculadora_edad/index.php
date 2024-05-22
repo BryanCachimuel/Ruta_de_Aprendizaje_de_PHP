3bb3825d8630c04449e2677634bb276080c9a3be
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-05-21 21:19:05 -0500

    Maquetado del ejercicio y funcionalidad con jquery

--- calculadora_edad/index.php
+++ calculadora_edad/index.php
@@ -11,8 +11,32 @@
 </head>
 <body>
 
-    
+    <div class="container">
+        <h3>Calcular edad Automaticaamente a partir de la fecha de nacimiento</h3>
+        <div class="row">
+            <div class="col-md-3">
+                <label for="calendario">Escribe tu fecha de nacimiento</label>
+                <input type="text" name="calendario" id="calendario" class="form-control input-sm" readonly>
+                
+                <span class="label label-success">
+                    <span id="edadCalculada"></span>
+                </span>
+                
+            </div>
+        </div>
+    </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#calendario").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: '1900:' + 2024,
+            dateFormat: "yy-mm-dd"
+        });
+    });
+</script>
