588addd1cf67c10edff4ec3283748c4d9a09dce3
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-05-30 22:18:27 -0500

    Validanddo una alerta de sweetalert que confirma el registro de un cliente

--- ventacarros/resources/views/cliente/index.blade.php
+++ ventacarros/resources/views/cliente/index.blade.php
@@ -1,10 +1,22 @@
 @extends('welcome')
 
 @section('content')
-    
-<button type="button" class="btn btn-primary mt-5" data-bs-toggle="modal" data-bs-target="#crearcliente">
-    Agregar Nuevo Cliente
-</button>
+
+    @if ($message = Session::get('mensaje'))
+        <script>
+            Swal.fire({
+                title: "Proceso Exitoso",
+                text: "{{ $message }}",
+                icon: "success",
+                showConfirmButton: false,
+                timer: 3500
+            });
+        </script>
+    @endif
+
+    <button type="button" class="btn btn-primary mt-5" data-bs-toggle="modal" data-bs-target="#crearcliente">
+        Agregar Nuevo Cliente
+    </button>
 
     <div class="table-responsive mt-5">
         <table class="table text-center">
@@ -23,27 +35,29 @@
             <tbody>
                 @foreach ($clientes as $cliente)
                     <tr class="">
-                        <td scope="row">{{$cliente->id_cliente}}</td>
-                        <td>{{$cliente->cedula_cliente}}</td>
-                        <td>{{$cliente->nombre_cliente}}</td>
-                        <td>{{$cliente->edad_cliente}}</td>
-                        <td>{{$cliente->correo_cliente}}</td>
-                        <td>{{$cliente->celular_cliente}}</td>
-                        <td>{{$cliente->direccion_cliente}}</td>
+                        <td scope="row">{{ $cliente->id_cliente }}</td>
+                        <td>{{ $cliente->cedula_cliente }}</td>
+                        <td>{{ $cliente->nombre_cliente }}</td>
+                        <td>{{ $cliente->edad_cliente }}</td>
+                        <td>{{ $cliente->correo_cliente }}</td>
+                        <td>{{ $cliente->celular_cliente }}</td>
+                        <td>{{ $cliente->direccion_cliente }}</td>
                         <td>
-                            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editarcliente{{$cliente->id_cliente}}"><i class="fa-solid fa-pen-to-square"></i></button>
-                            <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#eliminarcliente{{$cliente->id_cliente}}"><i class="fa-solid fa-trash-can"></i></button>
+                            <button class="btn btn-success" data-bs-toggle="modal"
+                                data-bs-target="#editarcliente{{ $cliente->id_cliente }}"><i
+                                    class="fa-solid fa-pen-to-square"></i></button>
+                            <button class="btn btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#eliminarcliente{{ $cliente->id_cliente }}"><i
+                                    class="fa-solid fa-trash-can"></i></button>
                         </td>
                     </tr>
-                <!-- invocando al modal de actualización marca -->
-                @include('cliente.info')
+                    <!-- invocando al modal de actualización marca -->
+                    @include('cliente.info')
                 @endforeach
             </tbody>
         </table>
     </div>
 
-<!-- invocando al modal de creacion marca -->
-@include('cliente.create')
-
-
+    <!-- invocando al modal de creacion marca -->
+    @include('cliente.create')
 @endsection
