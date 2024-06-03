c68f78a912a75cff31924a9132220aa1d38254d3
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-02 22:02:35 -0500

    Obteniendo la cantidad de clientes totales registrados en la base de datos

--- ventacarros/app/Http/Controllers/AdminController.php
+++ ventacarros/app/Http/Controllers/AdminController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Marca;
+use App\Models\Cliente;
 
 class AdminController extends Controller
 {
     public function index(){
         $marcas = Marca::all();
-        return view('datos.index',['marcas'=>$marcas]);
+        $clientes = Cliente::all();
+        return view('datos.index',['marcas'=>$marcas, 'clientes'=>$clientes]);
     }
 }
