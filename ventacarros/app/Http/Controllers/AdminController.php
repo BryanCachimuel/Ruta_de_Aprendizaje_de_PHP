ea1e3894965f649722367517c932f258453c728e
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-03 20:59:58 -0500

    Realizando una consulta para obtener la totalidad de los registros realizados de carros dentro de la base de datos

--- ventacarros/app/Http/Controllers/AdminController.php
+++ ventacarros/app/Http/Controllers/AdminController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Marca;
 use App\Models\Cliente;
+use App\Models\Carro;
 
 class AdminController extends Controller
 {
     public function index(){
         $marcas = Marca::all();
         $clientes = Cliente::all();
-        return view('datos.index',['marcas'=>$marcas, 'clientes'=>$clientes]);
+        $carros = Carro::all();
+        return view('datos.index',['marcas'=>$marcas, 'clientes'=>$clientes, 'carros'=>$carros]);
     }
 }
