06bd8ecca6d57dd92edbd43cfa1fc0387eb73860
BryanCachimuel <bryanloyo56@gmail.com>
2024-07-07 23:50:22 -0500

    Corrigiendo la extensión de los archivos de las vistas dentro de la función get_view

--- sistema_cotizador/app/functions.php
+++ sistema_cotizador/app/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function get_view($view_name){
-    $view = VIEWS.$view_name.'View.php';
+    $view = VIEWS.$view_name;
     // en caso de no existir la vista
     if(!is_file($view_name)){
         die('La vista no existe');
