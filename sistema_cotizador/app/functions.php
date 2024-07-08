f8b4c98c4ceed84cfcdecd9427030a89734dd29a
BryanCachimuel <bryanloyo56@gmail.com>
2024-07-07 23:09:11 -0500

    Función get_view para poder validar que una vista exista dentro del proyecto

---
+++ sistema_cotizador/app/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function get_view($view_name){
+    $view = VIEWS.$view_name.'View.php';
+    // en caso de no existir la vista
+    if(!is_file($view_name)){
+        die('La vista no existe');
+    }
+    // Existe la vista
+    require_once $view;
+}
