95c5b45ad721bd99ded59285ab80fdda76859d39
BryanCachimuel <bryanloyo56@gmail.com>
2024-07-07 22:48:20 -0500

    Definiendo constantes que incluyan en el header y footer

--- sistema_cotizador/app/config.php
+++ sistema_cotizador/app/config.php
@@ -1,7 +1,7 @@
 <?php
 
 /* validar si estamos en un sistema local o de producción */
-define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'],['127.0.0.1','::1']));
+define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']));
 
 /* definiendo la url para todo el sistema */
 $web_url = IS_LOCAL ? 'http://127.0.0.1/ejercicios_de_Repaso_PHP/sistema_cotizador/' : 'LA URL DE SU SERVIDOR EN PRODUCCIÓN';
@@ -10,12 +10,16 @@ define('URL', $web_url);
 
 /* Rutas para las carpetas en disco duro */
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', getcwd().DS);
-define('APP', ROOT.'app'.DS);
-define('ASSETS', ROOT.'assets'.DS);
-define('TEMPLATES',ROOT.'templates'.DS);
-define('INCLUDES',TEMPLATES.'includes'.DS);
-define('MODULES',TEMPLATES.'modules'.DS);
-define('VIEWS', TEMPLATES.'viiews'.DS);
+define('ROOT', getcwd() . DS);
+define('APP', ROOT . 'app' . DS);
+define('ASSETS', ROOT . 'assets' . DS);
+define('TEMPLATES', ROOT . 'templates' . DS);
+define('INCLUDES', TEMPLATES . 'includes' . DS);
+define('MODULES', TEMPLATES . 'modules' . DS);
+define('VIEWS', TEMPLATES . 'viiews' . DS);
+define('UPLOADS', ROOT . 'uploads' . DS);
 
-?>
+/*Para archivos que vayamos a incluir en header o footer (css o js) */
+define('CSS', URL . 'assets/css/');
+define('IMG', URL . 'assets/img/');
+define('JS', URL . 'assets/js/');
