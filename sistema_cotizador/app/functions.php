deb5c2f3f5164f59cbdc759a3eb34e572babb485
BryanCachimuel <bryanloyo56@gmail.com>
2024-07-08 23:14:26 -0500

    Agregando la sesiones para el proyecto

--- sistema_cotizador/app/functions.php
+++ sistema_cotizador/app/functions.php
@@ -9,3 +9,22 @@ function get_view($view_name){
     // Existe la vista
     require_once $view;
 }
+
+function get_quote(){
+    if(!isset($_SESSION['new_quote'])){
+        return $_SESSION['new_quote'] = [
+            'number' => rand(111111, 999999),
+            'name' => '',
+            'company' => '',
+            'email' => '',
+            'items' => [],
+            'subtotal' => 0,
+            'taxes' => 0,
+            'shipping' => 0,
+            'total' => 0
+        ];
+    }
+    recalculate_quote();
+
+    return $_SESSION['new_quote'];
+}
