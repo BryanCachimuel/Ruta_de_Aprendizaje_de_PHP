f2568a066f4ef1e186256f6b499ecc41d7525c20
BryanCachimuel <bryanloyo56@gmail.com>
2024-07-17 17:38:05 -0500

    Agregando una tarjeta para editar un concepto seleccionado

--- sistema_cotizador/templates/views/index.php
+++ sistema_cotizador/templates/views/index.php
@@ -1,48 +1,94 @@
-<?php require_once INCLUDES.'head.php' ?>
-<?php require_once INCLUDES.'navbar.php' ?>
+<?php require_once INCLUDES . 'head.php' ?>
+<?php require_once INCLUDES . 'navbar.php' ?>
 
 <!-- content -->
-     <div class="container-fluid py-5">
-        <div class="row">
-            <div class="col-12 wrapper_notifications">
+<div class="container-fluid py-5">
+    <div class="row">
+        <div class="col-12 wrapper_notifications">
 
-            </div>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-lg-7 col-12">
-                <div class="card mb-3">
-                    <div class="card-header">Información del Cliente</div>
-                    <div class="card-body">
-                        <form action="">
-                            <div class="form-group row">
-                                <div class="col-4">
-                                    <label for="nombre">Nombre</label>
-                                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Escriba su nombre" required>
-                                </div>
-                                <div class="col-4">
-                                    <label for="empresa">Empresa</label>
-                                    <input type="text" class="form-control" name="empresa" id="empresa" placeholder="Escriba el nombre de la empresa" required>
-                                </div>
-                                <div class="col-4">
-                                    <label for="email">Correo</label>
-                                    <input type="email" class="form-control" name="email" id="email" placeholder="Escriba su correo" required>
+    <div class="row">
+        <div class="col-lg-7 col-12">
+            <div class="card mb-3">
+                <div class="card-header">Información del Cliente</div>
+                <div class="card-body">
+                    <form action="">
+                        <div class="form-group row">
+                            <div class="col-4">
+                                <label for="nombre">Nombre</label>
+                                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Escriba su nombre" required>
+                            </div>
+                            <div class="col-4">
+                                <label for="empresa">Empresa</label>
+                                <input type="text" class="form-control" name="empresa" id="empresa" placeholder="Escriba el nombre de la empresa" required>
+                            </div>
+                            <div class="col-4">
+                                <label for="email">Correo</label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Escriba su correo" required>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">Agregar Nuevo Concepto</div>
+                <div class="card-body">
+                    <form id="add_to_quote" method="post">
+                        <div class="form-group row">
+                            <div class="col-3">
+                                <label for="concepto">Concepto</label>
+                                <input type="text" class="form-control" name="concepto" id="concepto" placeholder="Ingrese un concepto" required>
+                            </div>
+
+                            <div class="col-3">
+                                <label for="tipo">Tipo de Producto</label>
+                                <select name="tipo" id="tipo" class="form-control">
+                                    <option value="producto">Producto</option>
+                                    <option value="servicio">Servicio</option>
+                                </select>
+                            </div>
+
+                            <div class="col-3">
+                                <label for="cantidad">Cantidad</label>
+                                <input type="number" class="form-control" name="cantidad" id="cantidad" min="1" max="99999" value="1" required>
+                            </div>
+
+                            <div class="col-3">
+                                <label for="precio_unitario">Precio Unitario</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">$</span>
+                                    </div>
+                                    <input type="text" class="form-control" name="precio_unitario" id="precio_unitario" placeholder="0.00" required>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                        <br>
+                        <button class="btn btn-success" type="submit">Agregar Concepto</button>
+                        <button class="btn btn-danger" type="reset">Cancelar</button>
+                    </form>
                 </div>
+            </div>
 
-                <div class="card">
-                    <div class="card-header">Agregar Nuevo Concepto</div>
+        </div>
+
+        <div class="col-lg-5 col-12">
+
+            <div class="wrapper_update_concept d-none">
+                <div class="card mb-3">
+                    <div class="card-header">Editando Concepto</div>
                     <div class="card-body">
-                        <form id="add_to_quote" method="post">
+                        <form id="save_concept" method="post">
+                            <input type="hidden" class="form-control" name="id_concepto" id="id_concepto">
                             <div class="form-group row">
                                 <div class="col-3">
                                     <label for="concepto">Concepto</label>
                                     <input type="text" class="form-control" name="concepto" id="concepto" placeholder="Ingrese un concepto" required>
                                 </div>
-                                
+
                                 <div class="col-3">
                                     <label for="tipo">Tipo de Producto</label>
                                     <select name="tipo" id="tipo" class="form-control">
@@ -67,7 +113,7 @@
                                 </div>
                             </div>
                             <br>
-                            <button class="btn btn-success" type="submit">Agregar Concepto</button>
+                            <button class="btn btn-success" type="submit">Guardar Concepto</button>
                             <button class="btn btn-danger" type="reset">Cancelar</button>
                         </form>
                     </div>
@@ -75,24 +121,22 @@
 
             </div>
 
-            <div class="col-lg-5 col-12">
-               <div class="card">
+            <div class="card">
                 <div class="card-header">
-                    Resumen de Cotización 
+                    Resumen de Cotización
                     <button class="btn btn-danger float-right restart_quote">Reiniciar</button>
                 </div>
                 <div class="card-body wrapper_quote">
-                    
+
                 </div>
                 <div class="card-footer">
                     <button class="btn btn-primary">Descargar PDF</button>
                     <button class="btn btn-success">Enviar por Correo</button>
                 </div>
-               </div> 
             </div>
         </div>
-     </div>
-    <!-- end content -->
+    </div>
+</div>
+<!-- end content -->
 
-    <?php require_once INCLUDES.'footer.php' ?>
-
+<?php require_once INCLUDES . 'footer.php' ?>
