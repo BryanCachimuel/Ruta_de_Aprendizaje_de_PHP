0c6bab3fe985d78c199b914b301f80e01ba0744a
BryanCachimuel <bryanloyo56@gmail.com>
2024-07-23 21:44:13 -0500

    Se realiza una busqueda del archivo en el directorio donde se guardar los pdf de las cotizaciones

--- sistema_cotizador/pdf.php
+++ sistema_cotizador/pdf.php
@@ -5,3 +5,23 @@ require_once 'app/config.php';
 if(!isset($_GET['index'])){
     redirect('index.php?error=invalid_number');
 }
+
+// Si no hay cotizaciones
+$quotes = get_all_quotes();
+if(empty($quotes)){
+    redirect('index.php?error=no_quotes');
+}
+
+// Buscar el match del folio que buscamos
+$number = trim($_GET['number']);
+foreach($quotes as $q){
+   if(sprintf(UPLOADS.'coty_%s.pdf',$number) ==$q){
+     // Descarga
+     header('Content-Type: application/pdf');
+     header(sprintf('Content-Disposition: attachment;filename=%s', pathinfo($q, PATHINFO_BASENAME)));
+     readfile($q);
+   }
+}
+
+// No existe la cotización
+redirect('index.php?error=not_found');
