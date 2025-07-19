10ec77a37cf48cd3e0fa973ff70c107a556db0bf
BryanCachimuel <bryanloyo56@gmail.com>
2025-07-18 22:56:34 -0500

    Eliminando el archivo con el proceso de consulta de eliminación de un registro mediante su identificador

--- productos_php_api_fecth/src/eliminar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $data = file_get_contents("php://input");
-    require "../database/conexion.php";
-    $query = $pdo->prepare("DELETE FROM productos WHERE id = :id");
-    $query->bindParam(":id", $data);
-    $query->execute();
-    echo "ok";
-?>
